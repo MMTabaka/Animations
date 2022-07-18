cf70fa77157398d7b26e251baab7cb992910445a
Milosz Tabaka <miloszmtabaka@gmail.com>
2022-07-18 16:24:58 +0100

    Customising animations in SwiftUI

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -18,9 +18,20 @@ struct ContentView: View {
         .background(.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeOut(duration: 2)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount = 2
+        }
     }
 }
 
