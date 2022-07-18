573e7e1ab46e947a9e6c9933cdda00d8ae1709c0
Milosz Tabaka <miloszmtabaka@gmail.com>
2022-07-18 15:58:15 +0100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Milosz Tabaka on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
